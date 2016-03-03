c9660da80b9e519cf3e9224d9e8e21fde826aa1e
RuanXiaoZe <ruanxiaoze@outlook.com>
2016-03-03 22:31:21 +0800

    :smile: imooc-Dom事件探秘-抽奖效果的制作；
    跟着imooc-Amy老师制作；很有成就感；加油；

--- Demo 2016-3-3/choujiang/js/script.js
+++ Demo 2016-3-3/choujiang/js/script.js
@@ -1,17 +1,52 @@
-var data=['phone6','Ipad','三星笔记本','佳能相机','惠普打印机','谢谢参与','50元充值卡','1000元超市购物劵']，
-     timer=null;
+var data=['phone6','Ipad','三星笔记本','佳能相机','惠普打印机','谢谢参与','50元充值卡','1000元超市购物劵'],
+     timer=null,
+     flag=0;
 
 window.onload=function(){
-    var title=document.getElementById('title'),
-        play=document.getElementById('play'),
+    var play=document.getElementById('play'),
         stop=document.getElementById('stop');
 
     // 开始抽奖
     play.onclick=playFun; 
+    stop.onclick=stopFun;   
+
+    // 键盘事件 
+    document.onkeyup=function(event){
+        event=event || window.event;
+        if (event.keyCode==32) {
+            if (flag==0) {
+                playFun();
+                flag=1;
+            } else{
+                stopFun();
+                flag=0;
+            };
+        };  
+    }
 }
 
 function playFun () {
+    var that=this;
+    var title=document.getElementById('title');
+    var play=document.getElementById('play');
+    clearInterval(timer)
     timer=setInterval(function(){
-        console.log('hello');
-    },50)
+        var random=Math.floor(Math.random()*data.length);
+        title.innerHTML=data[random];
+    },50);
+    play.style.background='#999';
+    // this.style.background='#999';   此行敲空格有Bug；
 }
+
+function stopFun(){
+    clearInterval(timer);
+    var play=document.getElementById('play');
+    play.style.background='#036';
+ }
+
+
+
+
+
+
+
